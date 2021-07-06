7e09fe8783d8789b08768339fdd8eb5334ef8787
huda16 <mochamadnurulhuda16@gmail.com>
2021-07-06 14:09:26 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import 'regenerator-runtime' /* for async await transpile */
+import '../styles/main.css'
+import App from './views/app'
+import swRegister from './utils/sw-register'
+
+const app = new App({
+  button: document.querySelector('#drawer'),
+  drawer: document.querySelector('.nav'),
+  content: document.querySelector('#mainContent')
+})
+
+window.addEventListener('hashchange', () => {
+  app.renderPage()
+})
+
+window.addEventListener('load', () => {
+  app.renderPage()
+  swRegister()
+})
