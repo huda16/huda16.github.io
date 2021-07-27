c6af34982e317e682d5ca9ffac5b34afb6676c2f
huda16 <mochamadnurulhuda16@gmail.com>
2021-07-27 08:55:36 +0700

    update buat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoiaHVkYTE2IiwiYSI6ImNrcmplYTFkeDZ6N3YzMXBhMjM2dGM3ZGoifQ.4e4nl5NnRvPurHhhf_DE5Q";
 
-var map = new mapboxgl.Map({
-  container: "mapid", // container ID
-  style: "mapbox://styles/huda16/ckrjgm3cv0teu17qrx6vy6ju1", // style URL
-  center: [107.725, -6.94], // starting position [lng, lat]
-  zoom: 16, // starting zoom
-});
-
+const map1 = document.querySelector("#map");
+const map2 = document.querySelector("#mapbuat");
+const map3 = document.querySelector("#mapcari");
+let marker;
 // create the popup
-var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
   `<img class="img-thumbnail rounded-circle small" src="assets/img/undraw_profile.svg" width="64px">
   <h4>Penangkaran Kucing UPI</h4>
   <p>Penangkaran kucing ini berdiri sejak 1999</p>
@@ -18,11 +15,45 @@ var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
 );
 
 // create DOM element for the marker
-var el = document.createElement("div");
+const el = document.createElement("div");
 el.id = "marker";
 
-// create the marker
-var marker = new mapboxgl.Marker(el)
-  .setLngLat([107.725, -6.94])
-  .setPopup(popup)
-  .addTo(map);
+if (map1) {
+  const map = new mapboxgl.Map({
+    container: "mapid", // container ID
+    style: "mapbox://styles/huda16/ckrjgm3cv0teu17qrx6vy6ju1", // style URL
+    center: [107.725, -6.94], // starting position [lng, lat]
+    zoom: 16, // starting zoom
+  });
+
+  // create the marker
+  marker = new mapboxgl.Marker(el)
+    .setLngLat([107.725, -6.94])
+    .setPopup(popup)
+    .addTo(map);
+} else if (map2) {
+  const mapbuat = new mapboxgl.Map({
+    container: "mapbuat", // container ID
+    style: "mapbox://styles/huda16/ckrjgm3cv0teu17qrx6vy6ju1", // style URL
+    center: [107.725, -6.94], // starting position [lng, lat]
+    zoom: 16, // starting zoom
+  });
+
+  // create the marker
+  marker = new mapboxgl.Marker(el)
+    .setLngLat([107.725, -6.94])
+    .setPopup(popup)
+    .addTo(mapbuat);
+} else if (map3) {
+  const mapcari = new mapboxgl.Map({
+    container: "mapcari", // container ID
+    style: "mapbox://styles/huda16/ckrjgm3cv0teu17qrx6vy6ju1", // style URL
+    center: [107.725, -6.94], // starting position [lng, lat]
+    zoom: 16, // starting zoom
+  });
+  // create the marker
+  marker = new mapboxgl.Marker(el)
+    .setLngLat([107.725, -6.94])
+    .setPopup(popup)
+    .addTo(mapcari);
+}
