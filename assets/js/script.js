956f29ae51365b1934c904eb302c3d107870b6a4
huda16 <mochamadnurulhuda16@gmail.com>
2021-07-28 10:29:12 +0700

    tambah page maps dan update buat

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,18 +32,92 @@ if (map1) {
     .setPopup(popup)
     .addTo(map);
 } else if (map2) {
-  const mapbuat = new mapboxgl.Map({
-    container: "mapbuat", // container ID
-    style: "mapbox://styles/huda16/ckrjgm3cv0teu17qrx6vy6ju1", // style URL
-    center: [107.725, -6.94], // starting position [lng, lat]
-    zoom: 16, // starting zoom
+  var map = new mapboxgl.Map({
+    container: "mapbuat",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [117.97, -2.49],
+    zoom: 4,
   });
 
-  // create the marker
-  marker = new mapboxgl.Marker(el)
-    .setLngLat([107.725, -6.94])
-    .setPopup(popup)
-    .addTo(mapbuat);
+  /* Given a query in the form "lng, lat" or "lat, lng"
+   * returns the matching geographic coordinate(s)
+   * as search results in carmen geojson format,
+   * https://github.com/mapbox/carmen/blob/master/carmen-geojson.md */
+  var coordinatesGeocoder = function (query) {
+    // Match anything which looks like
+    // decimal degrees coordinate pair.
+    var matches = query.match(
+      /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+    );
+    if (!matches) {
+      return null;
+    }
+
+    function coordinateFeature(lng, lat) {
+      return {
+        center: [lng, lat],
+        geometry: {
+          type: "Point",
+          coordinates: [lng, lat],
+        },
+        place_name: "Lat: " + lat + " Lng: " + lng,
+        place_type: ["coordinate"],
+        properties: {},
+        type: "Feature",
+      };
+    }
+
+    var coord1 = Number(matches[1]);
+    var coord2 = Number(matches[2]);
+    var geocodes = [];
+
+    if (coord1 < -90 || coord1 > 90) {
+      // must be lng, lat
+      geocodes.push(coordinateFeature(coord1, coord2));
+    }
+
+    if (coord2 < -90 || coord2 > 90) {
+      // must be lat, lng
+      geocodes.push(coordinateFeature(coord2, coord1));
+    }
+
+    if (geocodes.length === 0) {
+      // else could be either lng, lat or lat, lng
+      geocodes.push(coordinateFeature(coord1, coord2));
+      geocodes.push(coordinateFeature(coord2, coord1));
+    }
+
+    return geocodes;
+  };
+
+  const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    localGeocoder: coordinatesGeocoder,
+    zoom: 16,
+    placeholder: "Search for places in Indonesia",
+    mapboxgl: mapboxgl,
+    reverseGeocode: true,
+    marker: true,
+    bbox: [
+      94.9106962020671, -11.1074231973731, 141.029649999987, 6.17575589605682,
+    ],
+  });
+
+  // Add the control to the map.
+  map.addControl(geocoder);
+
+  map.on("style.load", function () {
+    map.on("click", function (e) {
+      var coordinates = e.lngLat;
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(
+          `you clicked here: <br/> ${coordinates}
+          <button class="btn btn-primary" onclick="getLocation('${coordinates}')">Pilih</button>`
+        )
+        .addTo(map);
+    });
+  });
 } else if (map3) {
   const mapcari = new mapboxgl.Map({
     container: "mapcari", // container ID
@@ -56,4 +130,8 @@ if (map1) {
     .setLngLat([107.725, -6.94])
     .setPopup(popup)
     .addTo(mapcari);
+}
+
+function getLocation(locate) {
+  document.getElementById("lokasi").value = locate;
 }
